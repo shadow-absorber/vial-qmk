fd9b12d54f19dc0a52b16e62fe947891a8ba2859
shadow-absorber <sam.tunder@gmail.com>
2025-01-14 17:35:44 +0100

    adds support for ploopy mouse while not using tapdance

---
+++ keyboards/ploopyco/mouse/keymaps/vial/rules.mk
@@ -0,0 +1,15 @@
+# Processor frequency
+F_CPU = 8000000
+
+POINTING_DEVICE_DRIVER = pmw3360
+
+# add vial config
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+TAP_DANCE_ENABLE = no
+
+
+LTO_ENABLE = yes
+
+# VIAL_INSECURE = yes
